8ef7e253c00c8fd2bb518dd67cf030c3e8c50362
huxaiphaer <l654321123>
2018-10-01 14:10:18 +0300

    ch(setup-tests-environment): Implement testing environment. - Ensure that the application can be tested.
    [Delivers #160845487]:

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from "./App";
 
-const App = () => {
-    return <h3>Welcome to Authors Haven</h3>;
-};
 
- ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+//registerServiceWorker();
