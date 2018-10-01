33090690c523b5d09ad417c1f00168cfd4a789e1
Patrick Kimanje <patrickkimanje@Patricks-MacBook-Pro.local>
2018-10-01 15:06:46 +0300

    chore(Create React app): Create an empty React application
         - update the README.md
         - update the .gitignore
    
        [#160845485]

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <h3>Welcome to Authors Haven</h3>;
+};
+
+ ReactDOM.render(<App />, document.getElementById('root'));
