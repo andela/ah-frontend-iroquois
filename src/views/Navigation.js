6ca224d3ecb38e61933a03547af04f92d9c32dde
Patrick Kimanje <patrickkimanje@patricks-macbook-pro.local>
2018-10-03 14:23:45 +0300

    chore (auth-haven): Setup SASS processing on the client
    - enable sass compilation in react
    
    [Finishes  #160845490]

--- src/views/Navigation.js
+++ src/views/Navigation.js
@@ -2,12 +2,12 @@ import React from 'react';
 import {NavLink} from 'react-router-dom';
 
 const Navigation = () => {
-    return (
-        <div>
-            <NavLink to='/'>Home</NavLink>&nbsp;
-            <NavLink to='/login'>Login</NavLink>
-        </div>
-    );
+	return (
+		<div>
+			<NavLink to='/'>Home</NavLink>&nbsp;
+			<NavLink to='/login'>Login</NavLink>
+		</div>
+	);
 };
 
 export default Navigation;
