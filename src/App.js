e80af8ce5edc6351b0eef4163ea463e7ea596037
Colline Waitire <collinewaitire@Collines-MacBook-Pro.local>
2018-10-04 15:17:17 +0300

    chore(react-router): Setup react router version 4
         - install and configure the router
         - create dummy route for login
         - create the root page to test the configuration
         - create a components and views file structure
    
        [Finishes #160845489]

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Home from './views/Home';
+import Login from './views/Login';
+import Navigation from './views/Navigation';
 
 
-class App extends Component {
+const App = () => {
+    return (
+        <Router>
+            <div>
+            <Navigation />
+            <Switch>
+                <Route path='/' component={Home} exact />
+                <Route path='/login' component={Login} />
+            </Switch>
+            </div>
+        </Router>);
+};
 
-    render() {
-        return (
-            <div>Welcome to Authors Haven</div>
-
-    );
-    }
-}
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './redux-js/index';
 import {store} from "./redux-js/stores/index";
-import App from "./App";
-
+import App from './App';
 ReactDOM.render(
      <provider store={store}>
          <App />
