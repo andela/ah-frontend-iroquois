84eefa02d0e8379c0f4eedc694bb22d307ca451c
Estaer <estaerbaekar@gmail.com>
2018-10-21 19:47:20 +0300

    bg(authentication): fix bugs on login (#20)
    - remove irrelevant password validation
     - add token to local storage
     - reduce error message font sizes
    
     [Fixes #161281811]

--- src/redux-js/actions/LoginAction.js
+++ src/redux-js/actions/LoginAction.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import Login, {userLoginFail} from './LoginActionCreator';
 import {notify} from 'react-notify-toast';
 import requestLoadingAction from './request-loading';
-import API_URLS from '../../constants';
+import API_URLS, {AUTH_TOKEN} from '../../constants';
 
 export const userLoginRequest = (user) => {
 	window.store.dispatch(requestLoadingAction(true));
@@ -11,8 +11,11 @@ export const userLoginRequest = (user) => {
 		return await axios.post(API_URLS.LOGIN_URL, {user}
 		).then(response => {
 			if (response.status === 200) {
+				const token = response.data.user.token? response.data.user.token: null;
+				localStorage.setItem(AUTH_TOKEN, token);
 				window.store.dispatch(requestLoadingAction(false));
 				notify.show('You have successfully logged in.','success',4000);
+				window.location.assign('/');
 			}
 			dispatch(Login(response)
 			  );
